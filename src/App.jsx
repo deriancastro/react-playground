11aee74d4eb2537dde0fccb096f791cecc051719
Derian Castro <deriancastro@hotmail.com>
2021-05-13 19:30:25 +0200

    Add Radius

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,29 @@
 import React, { useState } from 'react'
 import { v4 as uuidv4 } from 'uuid'
 import './App.css'
+
 export default () => {
   const [size, setSize] = useState(100)
   const [rotation, setRotation] = useState(0)
   const [color, setColor] = useState(0)
+  const [radius, setRadius] = useState(0)
 
   const style = {
     width: size + 'px',
     height: size + 'px',
     transform: 'rotate(' + rotation + 'deg)',
     backgroundColor: color,
+    borderRadius: radius + 'px',
   }
 
   return (
     <div className="App">
       <label>
         Size:{' '}
-        <input value={size} onChange={handleChange} type="range" max="200" />
+        <input value={size} onChange={handleSize} type="range" max="200" />
       </label>
       <label>
-        Rotation:
+        Rotation:{' '}
         <input
           value={rotation}
           onChange={handleRotation}
@@ -29,19 +32,27 @@ export default () => {
         />
       </label>
       <label>
-        Color:
+        Radius:{' '}
+        <input value={radius} onChange={handleRadius} type="range" max="100" />
+      </label>
+      <label>
+        Color:{' '}
         <input value={color} onChange={handleColor} type="color" max="200" />
       </label>
       <div style={style} className="Box" />
     </div>
   )
-  function handleChange(event) {
+  function handleSize(event) {
     const input = event.target
     setSize(input.value)
   }
   function handleRotation(event) {
     const input = event.target
     setRotation(input.value)
+  }
+  function handleRadius(event) {
+    const input = event.target
+    setRadius(input.value)
   }
   function handleColor(event) {
     const input = event.target
